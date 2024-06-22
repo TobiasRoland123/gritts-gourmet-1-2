ff5ae860610f974ea6423233a7a7b3da78fa013f
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-06-22 17:47:14 +0200

    ...

--- src/modules/RecipeCarousel/RecipeCarousel.tsx
+++ src/modules/RecipeCarousel/RecipeCarousel.tsx
@@ -40,7 +40,7 @@ export function RecipeCarousel({ title, recipes }: RecipeCarouselViewModel) {
         <div className='flex justify-center'>
           <CarouselContent className='ml-6 md:ml-10 lg:ml-16 mr-4 xl:mr-0 xl:ml-auto'>
             {recipes?.map((recipe, index) => {
-              if (index >= 4) {
+              if (index >= 5) {
                 return null;
               }
               return (
