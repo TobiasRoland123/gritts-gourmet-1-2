bf0a9a3f6267d04799c008794fcd8d3dadba6338
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-06-26 22:13:54 +0200

    ...

--- src/pages/opskrifter/[slug]/index.tsx
+++ src/pages/opskrifter/[slug]/index.tsx
@@ -1,10 +1,55 @@
 import RecipeCard from '@/components/RecipeCard/RecipeCard';
+import { fetchEntries } from '../../../utils/contentFullPage';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
-const RecipeDetails = ({ params }: { params: { slug: string } }) => {
+const RecipeDetails = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+  const [recipe, setRecipe] = useState({} as any);
+  console.log('slug', slug);
+
+  const getRecipe = async () => {
+    console.log('getRecipe');
+
+    const data = slug ? await fetchEntries({ id: slug.toString() }) : null;
+    console.log('data', data);
+    if (!data) {
+      console.log('No data found');
+      return;
+    }
+    setRecipe(data);
+  };
+
+  useEffect(() => {
+    console.log('useEffect run');
+
+    if (slug) {
+      getRecipe();
+    }
+  }, [slug]); // Include slug in the dependency array
+
   return (
-    <div>
-      <h1>Recipe Details {params.slug}</h1>
-    </div>
+    <section>
+      {recipe?.billede?.fields?.file?.url ? (
+        <Image
+          src={'https:' + recipe.billede.fields.file.url}
+          alt='billede af retten'
+          width={1750}
+          height={500}
+        />
+      ) : null}
+      {recipe.title ? <h1> {recipe.title} </h1> : null}
+
+      {recipe.ingredienser ? (
+        <ul>
+          {recipe.ingredienser.map((ingredient: any, index: number) => (
+            <li key={`ingredient-${index}`}>{ingredient}</li>
+          ))}
+        </ul>
+      ) : null}
+    </section>
   );
 };
 
