b1357b6ba8dbb55fa37cd4fa84d39334fcca575b
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-07-06 01:18:12 +0200

    ...

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import Navigation from '@/components/Navigation/Navigation';
 import Footer from '@/components/Footer/Footer';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,6 +8,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Navigation
         navItems={[{ page: { name: 'opskrifter', path: '/opskrifter' } }, { page: { name: 'Om Gritt', path: '/om-gritt' } }]}
       />
+      <SpeedInsights />
+
       <main>{children}</main>
 
       <Footer />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <SpeedInsights />
     </Layout>
   );
 }
