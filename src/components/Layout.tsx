938785705e40e4862a29f6e5772dac571e44142b
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-07-06 01:09:51 +0200

    ...

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,6 @@
 import Navigation from '@/components/Navigation/Navigation';
 import Footer from '@/components/Footer/Footer';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: {
-    absolute: '',
-    default: "Gritt's Gourmet | Forside",
-    template: "%s | Gritt's Gourmet",
-  },
-  description:
-    "Gritt's Gourmet er en madblog med fokus på lækre opskrifter og madoplevelser. Find inspiration til din næste ret her!",
-};
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
